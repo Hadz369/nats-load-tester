8387a663730d49e2873f40b37cf731d978825ce7
Hadz369 <david@hadz.net>
2024-11-26 23:01:40 +1100

    added initial maui project

---
+++ src/NATSLoadTester/NATSLoadTester.Control/MainPageViewModel.cs
@@ -0,0 +1,41 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace NATSLoadTester.Control
+{
+    internal class MainPageViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        private DateTime _dateTime;
+        private Timer _timer;
+
+        public DateTime DateTime
+        {
+            get => _dateTime;
+            set
+            {
+                if (_dateTime != value)
+                {
+                    _dateTime = value;
+                    OnPropertyChanged(); // reports this property
+                }
+            }
+        }
+
+        public MainPageViewModel()
+        {
+            this.DateTime = DateTime.Now;
+
+            // Update the DateTime property every second.
+            _timer = new Timer(new TimerCallback((s) => this.DateTime = DateTime.Now),
+                               null, TimeSpan.Zero, TimeSpan.FromSeconds(1));
+        }
+
+        ~MainPageViewModel() =>
+            _timer.Dispose();
+
+        public void OnPropertyChanged([CallerMemberName] string name = "") =>
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+    }
+}
